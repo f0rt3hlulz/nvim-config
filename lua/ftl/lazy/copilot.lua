67ee30b221a5b6548a977ea4ec63be2eb077b47a
f0rt3hlulz <lulzm4k3r@gmail.com>
2025-05-21 09:53:41 +0400

    updated nvim config

---
+++ lua/ftl/lazy/copilot.lua
@@ -0,0 +1,22 @@
+return {
+   "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "InsertEnter",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept = "<C-l>",
+        next = "<M-]>",
+        prev = "<M-[>",
+      },
+    },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
